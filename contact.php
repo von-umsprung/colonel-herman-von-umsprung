d3646d5d85b136b9fd981f67ac30afaf5a444722
von-umsprung <testmobilesecu@gmail.com>
2020-07-09 11:10:09 +0200

    Add contact

---
+++ contact.php
@@ -0,0 +1,35 @@
+<?php include("common.php"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Colonel Herman Von Umsprung</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<center>
+
+<?php page_header("Nous contacter"); ?>
+		
+<div id="content">
+
+<div id="sidebar">
+<?php page_menu_full(); ?>
+</div>
+
+<div id="mainbar" class="mainbarstyle">
+
+<h1>Poser une question</h1> 
+
+<p>
+Si vous avez une question, veuillez vous référer au <a href="manuel.php">manuel</a>.</p>
+
+<?php page_footer(); ?>
+</div><!--mainbar-->
+</div><!--content-->
+
+</center>
+</body>
+</html>
