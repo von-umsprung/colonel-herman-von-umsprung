d5e3bf9d5fec60f9b243586901d976bc71eeaa02
von-umsprung <testmobilesecu@gmail.com>
2020-07-09 10:25:38 +0200

    Add content for organisation

---
+++ organisation.php
@@ -0,0 +1,66 @@
+<?php include("common.php"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<?php page_includes(); ?>
+<title>Colonel Herman Von Umsprung</title>
+</head>
+
+<body>
+<center>
+
+<?php page_header("Qui sommes nous ?"); ?>
+		
+<div id="content">
+
+<div id="sidebar">
+<?php page_menu_full(); ?>
+</div>
+
+<div id="mainbar" class="mainbarstyle">
+
+
+<h1>Organisation de la communauté</h1>
+
+<img width=100% src="img/nazis.jpg" alt="L'Association" class="logo-img">
+
+<table class="orga-table">
+
+<tr>
+<td style="width:50%">Organisation et coordination</td>
+<td>
+   Colonel Herman von Umsprung<br />
+   Colonel Gerhard Moeller<br />
+</td>
+</tr>
+
+<tr>
+<td>Responsable audiovisuel</td>
+<td>
+   Professeur Von Zimmel<br />
+</td>
+</tr>
+
+<tr>
+<td>Suivi des inscriptions</td>
+<td>
+   Heinrich von Zimmel<br />
+</td>
+</tr>
+
+<tr>
+<td>Stagiaire</td>
+<td>
+   Rubecht<br />
+</td>
+</tr>
+ </table>
+
+<?php page_footer(); ?>
+</div><!--mainbar-->
+</div><!--content-->
+
+</center>
+</body>
+</html>
