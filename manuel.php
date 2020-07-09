874dc2a955af31f289cd467ae4755a8db4147bbd
von-umsprung <testmobilesecu@gmail.com>
2020-07-09 11:09:18 +0200

    Add common questions

---
+++ manuel.php
@@ -0,0 +1,65 @@
+<?php include("common.php"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<?php page_includes(); ?>
+<title>Colonel Herman Von Umsprung</title>
+</head>
+
+<body>
+<center>
+
+<?php page_header("Manuel du concours"); ?>
+		
+<div id="content">
+
+<div id="sidebar">
+<?php page_menu_full(); ?>
+</div>
+
+<div id="mainbar" class="mainbarstyle">
+
+<div id="manuel">
+
+
+<h1>Condition de participation</h1>
+
+<h2>Y a-t-il des restrictions&nbsp;?</h2>
+<p>
+Il est nécessaire que les participants aient une taille minimale de 1m60. En effet, il a été dit : "Tu es un traître comme ta petite taille le laissait deviner !".
+</p>
+<h2>A qui s'adressent les épreuves&nbsp;?</h2>
+<p>
+Les épreuves sont ouvertes à tous. Elles seront seulement plus difficiles pour certains.
+</p>
+
+
+<h2>Quand les inscriptions sont-elles ouvertes&nbsp;?</h2>
+<p>Les inscriptions sont ouvertes toute l'année. N'hésitez pas à convier vos amis et connaissances.</p>
+
+<h2>Quelle est la date limite d'inscription&nbsp;?</h2>
+<p>Il n'y a pas de date limite d'inscription, et il sera encore possible de s'inscrire pendant la semaine du concours. Nous vous conseillons cependant de vous inscrire sans attendre.</p>
+
+
+<h1>Déroulement des épreuves</h1>
+
+<h2>Le concours demande-t-il une préparation des candidats&nbsp;?</h2>
+<p>
+Pas nécessairement. Aucun prérequis n'est nécessaire. Il est néanmoins nécessaire de montrer une grande motivation.
+</p>
+
+<h2>Quelle est la durée de l'épreuve&nbsp;?</h2>
+<p>
+Une journée maximum
+</p>
+</div><!--manuel-->
+
+
+<?php page_footer(); ?>
+</div><!--mainbar-->
+</div><!--content-->
+
+</center>
+</body>
+</html>
